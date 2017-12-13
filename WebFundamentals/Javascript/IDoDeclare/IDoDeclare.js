c30367984a3ad92e6020db0e39e6c131abc74f4f
Eddie Garcia <Eddie.Garcia@outlook.com>
2017-12-12 20:14:01 -0600

    initial checkin

---
+++ WebFundamentals/Javascript/IDoDeclare/IDoDeclare.js
@@ -0,0 +1,26 @@
+var num1, num2, num3, num4;
+
+num1 = 0.5;
+num2 = 25;
+num3 = -34;
+num4 = -234.5;
+
+var string1 = "I hate JaveScript.";
+var string2 = "I like JavaScript.";
+var string3 = "I love JavaScript.";
+
+var isTrue = true;
+var isFalse = false;
+
+var myUndefined;
+
+console.log("num1: ", num1);
+console.log("num2: ", num2);
+console.log("num3: ", num3);
+console.log("num4: ", num4);
+
+console.log("string1: ", string1);
+console.log("string2: ", string2);
+console.log("string3: ", string3);
+
+console.log("myUndefined: ", myUndefined);
